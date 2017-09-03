3817bbf02a2e721eb34aec279ba271e43b1cd55b
crowdzen <crowdzen>
2017-09-02 20:17:13 -0700

    adding structure

--- cp.sh
+++ cp.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+mkdir -p js
+mkdir -p css
+mkdir -p vendor/bootstrap/css
+mkdir -p vendor/font-awesome/css
+mkdir -p vendor/simple-line-icons/css
+mkdir -p device-mockups
+mkdir -p img
+
 cp ../startbootstrap-new-age/index.html .
-cp ../startbootstrap-new-age/js/new-age.min.js .
-cp ../startbootstrap-new-age/css/new-age.min.css .
-cp ../startbootstrap-new-age/vendor/bootstrap/css/bootstrap.min.css .
-cp ../startbootstrap-new-age/vendor/font-awesome/css/font-awesome.min.css .
-cp ../startbootstrap-new-age/vendor/simple-line-icons/css/simple-line-icons.css .
-cp ../startbootstrap-new-age/device-mockups/device-mockups.min.css .
-#cp ./startbootstrap-new-age/
-#cp ./startbootstrap-new-age/
-#cp ./startbootstrap-new-age/
-#cp ./startbootstrap-new-age/
-#cp ./startbootstrap-new-age/
+cp ../startbootstrap-new-age/js/new-age.min.js js/
+cp ../startbootstrap-new-age/css/new-age.min.css css/
+cp ../startbootstrap-new-age/vendor/bootstrap/css/bootstrap.min.css vendor/bootstrap/css/
+cp ../startbootstrap-new-age/vendor/font-awesome/css/font-awesome.min.css vendor/font-awesome/css/
+cp ../startbootstrap-new-age/vendor/simple-line-icons/css/simple-line-icons.css vendor/simple-line-icons/css/
+cp ../startbootstrap-new-age/device-mockups/device-mockups.min.css device-mockups/
+cp ../startbootstrap-new-age/img/demo-screen-1.jpg img/
+cp ../startbootstrap-new-age/img/*.svg img/
+#cp ../startbootstrap-new-age/
+#cp ../startbootstrap-new-age/
+#cp ../startbootstrap-new-age/
