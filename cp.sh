a330b95c3ed132da4f99b103a6cac300582a045d
crowdzen <crowdzen>
2017-09-02 20:11:00 -0700

    initial site

---
+++ cp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp ../startbootstrap-new-age/index.html .
+cp ../startbootstrap-new-age/js/new-age.min.js .
+cp ../startbootstrap-new-age/css/new-age.min.css .
+cp ../startbootstrap-new-age/vendor/bootstrap/css/bootstrap.min.css .
+cp ../startbootstrap-new-age/vendor/font-awesome/css/font-awesome.min.css .
+cp ../startbootstrap-new-age/vendor/simple-line-icons/css/simple-line-icons.css .
+cp ../startbootstrap-new-age/device-mockups/device-mockups.min.css .
+#cp ./startbootstrap-new-age/
+#cp ./startbootstrap-new-age/
+#cp ./startbootstrap-new-age/
+#cp ./startbootstrap-new-age/
+#cp ./startbootstrap-new-age/
